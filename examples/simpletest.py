3e7a20079d1377a6c14d672740213a687bcc9449
Tony DiCola <tony@tonydicola.com>
2016-04-20 19:52:25 +0000

    Fix busnum in example.

--- examples/simpletest.py
+++ examples/simpletest.py
@@ -12,7 +12,7 @@ dac = Adafruit_MCP4725.MCP4725()
 
 # Note you can change the I2C address from its default (0x62), and/or the I2C
 # bus by passing in these optional parameters:
-#dac = Adafruit_MCP4725.MCP4725(address=0x49, bus=1)
+#dac = Adafruit_MCP4725.MCP4725(address=0x49, busnum=1)
 
 # Loop forever alternating through different voltage outputs.
 print('Press Ctrl-C to quit...')
